93beedf4a46a011b3c90a682ac4d1f3c9e25a201
constant <constantduckens@yahoo.fr>
2014-03-06 07:30:20 -0800

    my django app

--- esihapp/views.py
+++ esihapp/views.py
@@ -322,7 +322,7 @@ def login(request):
             else:
                 return render(request,'esihapp/formerno.html',locals())
         except:
-             return redirect('../')
+             return render(request,'esihapp/formerno.html',locals())
 
     except KeyError:
         return render(request,'esihapp/form.html',locals())
